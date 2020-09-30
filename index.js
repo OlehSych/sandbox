fcddc8a0dfefabd0ac6fb32828088c426ef6992a
oleh.sych <olehsych@gmail.com>
2020-10-01 00:54:53 +0300

    upd (deepCopy): implement copy function

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ export function isObject(item) {
   return (typeof item === 'object') && !Array.isArray(item);
 }
 
+export function copyFunction(fn) {
+  const temp = function temporary(...args) {
+    return fn.apply(this, args);
+  };
+
+  Object
+    .keys(fn)
+    .forEach((key) => {
+      temp[key] = fn[key];
+    });
+
+  return temp;
+}
+
 export default {
   isObject,
+  copyFunction,
 };
--- src/deepCopy.js
+++ src/deepCopy.js
@@ -1,12 +1,15 @@
-import { isObject } from '..';
+import { isObject, copyFunction } from '..';
 
 export default function deepCopy(obj) {
   return Object.keys(obj).reduce((newObj, key) => {
     let newVal;
+
     if (isObject(obj[key])) {
       newVal = deepCopy(obj[key]);
     } else if (Array.isArray(obj[key])) {
       newVal = obj[key].map(item => (typeof item === 'object' ? deepCopy(item) : item));
+    } else if (typeof obj[key] === 'function') {
+      newVal = copyFunction(obj[key]);
     } else {
       newVal = obj[key];
     }

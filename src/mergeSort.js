8edafab6520497bcad1ae47d414de04643799525
oleh.sych <olehsych@gmail.com>
2020-10-03 17:38:02 +0300

    upd (mergeSort): add recursive split parts

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,4 +1,30 @@
 import randArray from './randArray';
-// TODO: implement merge sort
 
-console.log(randArray(50, -2.4444, 77, 1));
+const randArr = randArray(10, 0, 100);
+
+function compareAsc(v1, v2) {
+  return v1 < v2;
+}
+
+export default function mergeSort(arr, compare) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const splitInd = Math.floor(arr.length / 2);
+  const part1 = mergeSort(arr.slice(0, splitInd));
+  const part2 = mergeSort(arr.slice(splitInd));
+  const newArr = [];
+
+  for (let i = 0; i < part1.length; i++) {
+    for (let j = 0; j < part2.length; j++) {
+      if (compare(part1[i], part2[j])) {
+        newArr.push(part1[i]);
+      }
+    }
+  }
+
+  return newArr;
+}
+
+console.log(mergeSort(randArr, compareAsc));

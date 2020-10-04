7dc67183161ffaebde4d962a6742d7b3f4abf2a9
oleh.sych <olehsych@gmail.com>
2020-10-04 16:59:53 +0300

    add (mergeSort.test): add tests for mergeSort fn

---
+++ tests/mergeSort.test.js
@@ -0,0 +1,22 @@
+import mergeSort from '../src/mergeSort';
+
+describe('Array sorting using merge sort method', () => {
+  const arr = [1, -5, 3, -6, -43, 12, 7];
+  const sortedAscArr = [-43, -6, -5, 1, 3, 7, 12];
+  const sortedDescArr = [12, 7, 3, 1, -5, -6, -43];
+  const sortedAbsArr = [1, 3, -5, -6, 7, 12, -43];
+  const compareDesc = (v1, v2) => v1 > v2;
+  const compareAbs = (v1, v2) => Math.abs(v1) < Math.abs(v2);
+
+  it('Should sort the array ascending', () => {
+    expect(mergeSort(arr)).toEqual(sortedAscArr);
+  });
+
+  it('Should sort the array descending', () => {
+    expect(mergeSort(arr, compareDesc)).toEqual(sortedDescArr);
+  });
+
+  it('Should sort the array absolutely', () => {
+    expect(mergeSort(arr, compareAbs)).toEqual(sortedAbsArr);
+  });
+});

9e0c1fb0d90bb93a4ad249f38a39e1c7089eac6c
oleh.sych <olehsych@gmail.com>
2020-10-19 00:35:49 +0300

    add(linkedList): implement linked list functionality

---
+++ src/linkedList.js
@@ -0,0 +1,85 @@
+import LinkedListNode from './linkedListNode';
+
+/*
+Following are the basic operations supported by a list:
+Insertion − Adds an element at the beginning of the list.
+Deletion − Deletes an element at the beginning of the list.
+Display − Displays the complete list.
+Search − Searches an element using the given key.
+Delete − Deletes an element using the given key.
+ */
+
+function validateFunction(fn) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('Given parameter is not a function');
+  }
+}
+
+function validateIndex(i, length) {
+  if (i !== Math.floor(i)) {
+    throw new Error('Node index must be integer');
+  }
+
+  if (i < 0 || i > length) {
+    throw new Error('Out of range index');
+  }
+}
+
+export default class LinkedList {
+  constructor() {
+    Object.defineProperty(this, 'head', { writable: true });
+    this.head = null;
+    this.length = 0;
+  }
+
+  forEach(fn) {
+    validateFunction(fn);
+
+    for (let node = this.head; node !== null; node = node.next) {
+      fn(node.value);
+    }
+  }
+
+  map(fn) {
+    validateFunction(fn);
+
+    const results = [];
+
+    for (let node = this.head; node !== null; node = node.next) {
+      results.push(fn(node.value));
+    }
+
+    return results;
+  }
+
+  find(fn) {
+    validateFunction(fn);
+
+    for (let node = this.head; node !== null; node = node.next) {
+      if (fn(node.value)) {
+        return node.value;
+      }
+    }
+    return undefined;
+  }
+
+  add(val, i = this.length) {
+    validateIndex(i, this.length);
+
+    const newNode = new LinkedListNode(val);
+
+    if (i === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    if (i === this.length) {
+      this.head.getTail().next = newNode;
+    }
+
+    // TODO add item in the middle of list
+
+    this.length += 1;
+    return this;
+  }
+}
---
+++ tests/linkedList.test.js
@@ -0,0 +1,5 @@
+import LinkedList from '../src/linkedList';
+
+const test = new LinkedList();
+
+test.add(1).add(423).add(43231).add(-42313);

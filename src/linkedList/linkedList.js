813524e3be4ca8a9d08a53acc1a811b1f463d830
oleh.sych <olehsych@gmail.com>
2020-11-08 19:06:42 +0200

    upd(linkedList): add indexers, add tests

---
+++ src/linkedList/linkedList.js
@@ -0,0 +1,151 @@
+import LinkedListNode from './linkedListNode';
+import LinkedListIterator from './linkedListIterator';
+
+/*
+Following are the basic operations supported by a list:
+Insertion − Adds an element at the beginning of the list.
+Deletion − Deletes an element at the beginning of the list.
+Display − Displays the complete list.
+Search − Searches an element using the given key.
+Delete − Deletes an element using the given key.
+ */
+
+function validateFunction(fn) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('Given parameter is not a function');
+  }
+}
+
+function validateIndex(i, length) {
+  if (i !== Math.floor(i)) {
+    throw new TypeError('Node index must be integer');
+  }
+
+  if (i < 0 || i > length) {
+    throw new RangeError('Out of range index');
+  }
+}
+
+export default class LinkedList {
+  constructor() {
+    Object.defineProperty(this, 'head', { writable: true });
+    this.head = null;
+    this.length = 0;
+  }
+
+  [Symbol.iterator]() {
+    return new LinkedListIterator(this);
+  }
+
+  get(i) {
+    if (i === 0) {
+      return this.head.value;
+    }
+
+    if (i > 0 && i < this.length) {
+      for (let node = this.head.next, j = 1; node !== null; node = node.next, j++) {
+        if (i === j) {
+          return node.value;
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+  forEach(fn) {
+    validateFunction(fn);
+
+    for (let node = this.head; node !== null; node = node.next) {
+      fn(node.value);
+    }
+  }
+
+  map(fn) {
+    validateFunction(fn);
+
+    const results = [];
+
+    for (let node = this.head; node !== null; node = node.next) {
+      results.push(fn(node.value));
+    }
+
+    return results;
+  }
+
+  find(fn) {
+    validateFunction(fn);
+
+    for (let node = this.head; node !== null; node = node.next) {
+      if (fn(node.value)) {
+        return node.value;
+      }
+    }
+    return undefined;
+  }
+
+  includes(val) {
+    return !!this.find(v => v === val);
+  }
+
+  add(val, i = this.length) {
+    validateIndex(i, this.length);
+
+    const newNode = new LinkedListNode(val);
+
+    if (i === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else if (i === this.length) {
+      this.head.getTail().next = newNode;
+    } else {
+      for (let j = 1, currNode = this.head; j <= i; j++, currNode = currNode.next) {
+        if (i === j) {
+          newNode.next = currNode.next;
+          currNode.next = newNode;
+          break;
+        }
+      }
+    }
+
+    this.length += 1;
+    return this;
+  }
+
+  delete(val, all = false) {
+    if (this.head) {
+      if (this.head.value === val) {
+        this.head = this.head.next;
+        this.length -= 1;
+      } else {
+        for (let node = this.head; node !== null; node = node.next) {
+          if (node.next && node.next.value === val) {
+            node.next = node.next.next;
+            this.length -= 1;
+            if (!all) {
+              break;
+            }
+          }
+        }
+      }
+    }
+    return this;
+  }
+
+  deleteIndex(i) {
+    validateIndex(i, this.length - 1);
+
+    if (i === 0) {
+      this.head = this.head.next;
+    } else {
+      for (let node = this.head, j = 1; node !== null; node = node.next, j++) {
+        if (i === j) {
+          node.next = node.next.next;
+          break;
+        }
+      }
+    }
+    this.length -= 1;
+    return this;
+  }
+}
--- tests/linkedList.test.js
+++ tests/linkedList.test.js
@@ -1,77 +1,49 @@
 import LinkedList from '../src/linkedList';
 
-const testList = new LinkedList().add(2)
-  .add(3)
-  .add(4);
+describe('Test linked list implementation', () => {
+  let testList;
 
-console.log(testList.get(1));
-//
-// describe('Test linked list implementation', () => {
-//   let testList;
-//
-//   beforeEach(() => {
-//     testList = new LinkedList().add(2).add(3).add(4);
-//   });
-//
-//   it('Should add new node in the middle of list', () => {
-//     const index = 2;
-//     const value = 5;
-//     const { length } = testList;
-//     testList.add(value, index);
-//     expect(testList[index]).toEqual(value);
-//     expect(testList.length).toEqual(length + 1);
-//   });
-//
-//   it('Should delete nodes by value', () => {
-//     const expectedLength = 2;
-//     expect(testList.delete(3).length).toEqual(expectedLength);
-//     expect(testList.includes(3)).toBeFalsy();
-//     expect(testList.delete(2).delete(4).head).toBeNull();
-//   });
-//
-//   it('Should delete node by index', () => {
-//     const expectedLength = 2;
-//     expect(testList.deleteIndex(0).length).toEqual(expectedLength);
-//     expect(testList.includes(2)).toBeFalsy();
-//     expect(testList.deleteIndex(1).deleteIndex(0).head).toBeNull();
-//   });
-//
-//   it('Should find node by condition', () => {
-//     const isOddNumber = num => num % 2;
-//     expect(testList.find(isOddNumber)).toEqual(3);
-//   });
-//
-//   it('Should iterate nodes', () => {
-//     // eslint-disable-next-line no-restricted-syntax
-//     for (const item of testList) {
-//       console.log(item);
-//     }
-//     testList.add(2).add(3).add(4);
-//     const isOddNumber = num => num % 2;
-//     expect(testList.find(isOddNumber)).toEqual(3);
-//   });
-// });
+  beforeEach(() => {
+    testList = new LinkedList().add(2).add(3).add(4);
+  });
 
-class BaseModel {
-  constructor(attr) {
-    this.attr = attr;
+  it('Should add new node in the middle of list', () => {
+    const index = 2;
+    const value = 5;
+    const { length } = testList;
+    testList.add(value, index);
+    expect(testList.get(index)).toEqual(value);
+    testList[index] = value;
+    expect(testList[index]).toEqual(value);
+    expect(testList.length).toEqual(length + 2);
+  });
 
-  }
+  it('Should delete nodes by value', () => {
+    const expectedLength = 2;
+    expect(testList.delete(3).length).toEqual(expectedLength);
+    expect(testList.includes(3)).toBeFalsy();
+    expect(testList.delete(2).delete(4).head).toBeNull();
+  });
 
-  getValue(name) {
-    return this.attr[name] || '';
-  }
+  it('Should delete node by index', () => {
+    const expectedLength = 2;
+    expect(testList.deleteIndex(0).length).toEqual(expectedLength);
+    expect(testList.includes(2)).toBeFalsy();
+    expect(testList.deleteIndex(1).deleteIndex(0).head).toBeNull();
+  });
 
-  setValue(name, value) {
-    this.attr[name] = value;
-  }
-}
-// TODO: implement indexers in linkedList
+  it('Should find node by condition', () => {
+    const isOddNumber = num => num % 2;
+    expect(testList.find(isOddNumber)).toEqual(3);
+  });
 
-const p = new Proxy(new BaseModel(), {
-  get: (target, name) => target.getValue(<string>name),
-  set: (target, name, value) => {
-    target.setValue(name, value);
-    return true;
-  },
+  it('Should iterate nodes', () => {
+    let i = 0;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of testList) {
+      expect(item).toBe(testList[i]);
+      i += 1;
+    }
+    expect(i).toBe(testList.length);
+  });
 });

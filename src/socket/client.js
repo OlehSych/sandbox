22d3deb6e4c978953f7fc4cf923ee1abb1abee9b
s.oleh <s.oleh@improveit.solutions>
2021-05-21 01:52:22 +0300

    refactor: use simple example of socket.io with auth token sending

--- src/socket/client.js
+++ src/socket/client.js
@@ -1,14 +1,15 @@
-import { io } from 'socket.io-client';
+import io from 'socket.io-client';
 
-const socket = io('ws://localhost:3000', {
-  query: {
-    token: 'test',
-  },
-});
+const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJzX3BoeXNpcXVlX2lkIjo1MTcsIm1lbWJlcl9ncm91cF9pZCI6MSwiaWF0IjoxNjE4ODM5NjI5LCJleHAiOjE2MTg5MjYwMjl9.PUBkiBRThuEoXRvq3Cozoc_GxFw7qO22so5tzwDeO7k';
+const socket = io('http://localhost:3000', { auth: { token } });
 
 socket
   .on('connect', () => {
     console.log('CONNECT');
+    socket.send({
+      message: 'Client connected',
+      room: 'TestRoom',
+    });
   })
   .on('error', (error) => {
     console.log('ERROR', error);
@@ -16,8 +17,10 @@ socket
   .on('disconnect', (reason) => {
     console.log('DISCONNECT', reason);
   })
-  .on('message', data => console.log('message', data));
-
-setInterval(() => {
-  socket.send('Client connected');
-}, 1000);
+  .on('unauthorized', (err) => {
+    console.log('There was an error with the authentication:', err);
+  })
+  .on('exception', (err) => {
+    console.log('Exception:', err);
+  })
+  .on('message', data => console.log('message received: ', data));
--- src/socket/server.js
+++ src/socket/server.js
@@ -2,20 +2,24 @@ import { Server } from 'socket.io';
 
 const server = new Server(3000);
 
-server.use((socket, next) => {
-  const { token } = socket.handshake.query;
-  console.log('>>>>>>>>>', token);
-  return token.length
-    ? next()
-    : next(new Error('authentication error')); // TODO: implement error handler for the client
-});
+server
+  .on('connect', (socket) => {
+    const { token } = socket.handshake.auth;
 
-server.on('connect', (socket) => {
-  socket.on('message', (message) => {
-    console.log('message', message);
-    socket.emit('error', message);
+    if (!token.length) {
+      socket.emit('unauthorized', 'authentication error');
+      socket.disconnect();
+      return;
+    }
+
+    socket
+      .on('message', (message) => {
+        console.log('message', message);
+      })
+      .on('error', (error) => {
+        console.log('ERROR', error);
+      })
+      .on('message_created', (message) => {
+        console.log('message_created', message);
+      });
   });
-  socket.on('error', (error) => {
-    console.log('ERROR', error);
-  });
-});
